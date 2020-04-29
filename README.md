29298ada548fdfdcaa67250c1d94fa1d9953bcb2
sasezaki <sasezaki@gmail.com>
2020-04-29 12:52:28 +0900

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+demonstration for usage of stub for static analyser.
+======
+
+### phpstan
+```
+./vendor/bin/phpstan analyse --level 5 src/
+```
+
+### psalm
---
+++ src/Foo.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Demo;
+
+use Psr\Log\LoggerInterface;
+
+class Foo
+{
+    /** @var LoggerInterface */
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    public function error(\Throwable $throwable) : void
+    {
+        $this->logger->debug('foo', ['exception' => $throwable]);
+    }
+}
